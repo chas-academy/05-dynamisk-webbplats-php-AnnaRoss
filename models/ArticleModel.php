1d5fa714d125cd150e468e9df55d7aebe93fe869
Anna Ross <anna.ross@chasacademy.se>
2017-11-23 14:19:14 +0100

    Create a custom model ArticleModel
    At this point, the article model is quite spartan.

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -1,15 +1,22 @@
 <?php
 
+require_once('./core/Model.php');
+
 class ArticleModel extends Model 
 {
     protected static $tableName;
     protected $schema;
 
-    public function __construct(string $headline, string $runningtext) 
+    public function __construct(
+        string $headline, 
+        string $runningtext
+        // tbd way to attach category
+        // tbd way to attach tag/tags 
+    )
     {
         $this->schema = [
             'headline' => $headline,
-            'runningtext' => $runningtext
+            'runningtext' => $runningtext,
         ];
     }
 
@@ -17,6 +24,7 @@ class ArticleModel extends Model
     {
         static::$tableName = 'articles';
     }
+
 }
 
 ArticleModel::register();
