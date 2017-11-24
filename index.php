9c3e2ad7f41cb6d34652e4c4e8af3d9a5858e961
Anna Ross <anna.ross@chasacademy.se>
2017-11-24 16:07:33 +0100

    Prepare user login and request authentication
    User related:
    I. Start making use of Flesspress' Session class.
    II. Create route/controller for user login
    III. Create route/controller for limited access sections.
    IV. Create route/controller for reading column username from table users.
    V. Create route/controller for creating new users.
    
    Tag related:
    VI. Modify route/controller: set relation between article and
    tag (singluar atm) when creating a new blogpost.
    VII. Create route/controller for reading and creating tags.

--- index.php
+++ index.php
@@ -8,30 +8,34 @@
     /* Include Fleshpress */
     require_once('./core/Fleshpress.php');
 
+    /* Include Session */
+    require_once('./middleware/Session.php');
+
     /* Include custom models */
     require_once('./models/ArticleModel.php');
     require_once('./models/CategoryModel.php');
     require_once('./models/TagModel.php');
     require_once('./models/UserModel.php');
     require_once('./models/ArticleCategoryModel.php');
-    // require_once('./models/ArticleTagModel.php');
+    require_once('./models/ArticleTagModel.php');
     // require_once('./models/ArticleUserModel.php');
     
     /* Instantiate Fleshpress */
     $app = new Fleshpress();
+    $app->use(new Session);
 
     /* Register Routes and Controllers */
     $app->get('/', function($req, $res)
     {
         $properties = [
-            'title' => 'Starturu!',
-            'content' => 'This is the start page'
+            'sitetitle' => 'Blogeru <3',
+            'content' => 'This is starturupageuru!'
         ];
 
         $res->render_template('start.html', $properties);
     });
-
-    /* User log in */
+    
+    /***************** user ***********************/
     $app->post('/login', function($req, $res)
     {   
         $username = $req->body['username'];
@@ -40,18 +44,64 @@
         // hitta ett username med username
         // make use of fleshpress's session thingy - check if the user is of type 'admin' or 'regular'
         // last step: if login succeds, redirect to editing views!
+        $req->session->user = $userThatWasLoggedIn;
+
         $res->redirect('/');
+    });
+    // if req->session->user - tillgänliga vid alla routes
+    // if req->session->user - sätta till null vid inloggning. 
+
+    $app->get('/edit', 'checkAccessRights', function($req, $res)
+    {
+        $res->redirect('/edit');
+        
+    });
+
+    function checkAcessRights($req, $res) {
+        $hasAccessRights = false;
+
+        if (! $hasAccessRights) {
+            $res->json(["message" => "You shall not pass"], 401); // status code 401 - request is unauthorized
+        }
+    }
+
+
+
+    $app->get('/users', function($req, $res)
+    {
+        $users = UserModel::findAll();
+        $properties = [
+            'username' => $username,
+        ];
+
+        $res->render_template('users.html', ['users' => $users]);
     });
 
 
+    $app->post('/users', function ($req, $res)
+    {
+        $newUser = new UserModel(
+            $req->body['username'],
+            $req->body['email'],
+            $req->body['password']
+        ); 
+
+        $returnedUser = $newUser->save();
+        
+        $res->redirect('/users');
+    });
+
+    /***************** articles ***********************/
     $app->get('/articles', function($req, $res)
     {
         $articles = ArticleModel::findAll();
         $categories = CategoryModel::findAll();
+        $tags = TagModel::findAll();
 
         $res->render_template('articles.html', [
             'articles' => $articles,
-            'categories' => $categories
+            'categories' => $categories,
+            'tags' => $tags
         ]);
     });
 
@@ -73,11 +123,19 @@
         // assigna choosen category to $articleReturn. plocka ut returned article id med nyckel-brackets.
         // plocka ut nyckel id för categorin
         //nya upp en instans av Article_Category, mata in id-värdena (vilket ska definieras i Article_CategoryModels consturcotr. och spara den.
-        // 3. Save tags 
-        // Just as above, but I will need a loop if there is multiple tags.
+        // 3. Save tags Just as above, but I will need a loop if there is multiple tags
+        $newArticleTag = new ArticleTagModel(
+            $returnedArticle['id'],
+            $req->body['tag']
+        );
+
+        $newArticleTag->save();
+
         $res->redirect('/articles');
     });
 
+    
+    /**************** categories *********************/
 
     $app->get('/categories', function($req, $res)
     {
@@ -95,7 +153,28 @@
 
         $returnedCategory = $newCategory->save();
         
-        $res->redirect('/categories');
+        $res->redirect('/articles');
+    });
+
+
+    /***************** tags ***********************/
+
+    $app->get('/tags', function($req, $res)
+    {
+        $tags = TagModel::findAll();
+
+        $res->render_template('tags.html', ['tags' => $tags]);
+    });
+
+    $app->post('/tags', function ($req, $res)
+    {
+        $newTag = new TagModel(
+            $req->body['title']
+        ); 
+
+        $returnedTag = $newTag->save();
+        
+        $res->redirect('/articles');
     });
 
     /* Start the App */
